b6294d589e01809a6325c2af25b132575464f125
David Clarke <136286357+MICR0POP@users.noreply.github.com>
2023-06-14 21:03:41 -0600

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import requests
 from lxml import html
 from tqdm import tqdm
 
@@ -17,10 +19,12 @@ def check_username(username):
     tree = html.fromstring(response.content)
     user_icon = tree.xpath('//*[@class="tgme_icon_user"]')
     return len(user_icon) > 0
+
 def main():
     valid_usernames = []
     file_path = os.path.join(os.path.dirname(__file__), "list.txt")
     valid_file_path = os.path.join(os.path.dirname(__file__), "valid.txt")
+    
     # Verificar si el archivo existe
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
@@ -29,13 +33,16 @@ def main():
             for username in progress_bar:
                 if check_username(username):
                     valid_usernames.append(username)
+        
         with open(valid_file_path, "w") as valid_file:
             for username in valid_usernames:
                 valid_file.write(username + "\n")
+        
         print("Valid Usernames:")
         for username in valid_usernames:
             print(username)
     else:
         print(f"El archivo '{file_path}' no existe.")
+
 if __name__ == "__main__":
     main()
